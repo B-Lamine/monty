7c52b9059a624cc69ab0109bb1e139e00bc67905
Lamine Bellilet <medlamine.bellilet@outlook.com>
2023-06-22 13:03:54 -0700

    push: remove error message

--- main.c
+++ main.c
@@ -37,10 +37,8 @@ int main(int ac, char *av[])
 			fclose(fp);
 			exit_point(3, line_number, ar[0]);
 		}
-		if (ar[1] != NULL)
+		if ((ar[1] != NULL) && (strspn(ar[1], ALPHABET) == 0))
 			data = atoi(ar[1]);
-		if ((ar[1] == NULL) || (data == 0))
-			exit_point(4, line_number, NULL);
 		operation(&stack, line_number);
 		line_number++;
 	}
